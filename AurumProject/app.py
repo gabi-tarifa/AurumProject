bd5affb55db3d63889b9c06ac12445a2806ada28
MeowTarifa <126773354+gabi-tarifa@users.noreply.github.com>
2025-07-08 10:33:14 -0300

    Update app.py

--- AurumProject/app.py
+++ AurumProject/app.py
@@ -102,4 +102,4 @@ def efetuar_login():
 if __name__ == "__main__":
     #app.run(debug=True)
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
