86fd03001625876c883f1d0c45278865cd9e6638
gabi-tarifa <gabriel.tarifa.de.oliveira@gmail.com>
2025-07-28 13:58:26 -0300

    correcao adicao tabelas bdd

--- AurumProject/aurum.sql
+++ AurumProject/aurum.sql
@@ -76,10 +76,9 @@ CREATE TABLE RankingUsuario (
     FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario)
 );
 
-ALTER TABLE Usuario (
+ALTER TABLE Usuario 
     ADD COLUMN pontos INT NOT NULL DEFAULT 0,
-    ADD COLUMN profilepicture VARCHAR(255) NOT NULL DEFAULT 'https://cdn-icons-png.flaticon.com/256/149/149071.png'
-);
+    ADD COLUMN profilepicture VARCHAR(255) NOT NULL DEFAULT 'https://cdn-icons-png.flaticon.com/256/149/149071.png';
 
 DELIMITER //
 
