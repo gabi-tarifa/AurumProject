fd1d52055b00e26ee7b45fbf9535c1a541804f03
MeowTarifa <126773354+gabi-tarifa@users.noreply.github.com>
2025-08-08 15:22:56 -0300

    Tentativa de ressurreicao do projeto

--- AurumProject/static/js/perfil.js
+++
@@ -1,8 +0,0 @@
-
-  function abrirEdicao() {
-    document.getElementById('editarPerfil').classList.remove('oculto');
-  }
-
-  function fecharEdicao() {
-    document.getElementById('editarPerfil').classList.add('oculto');
-  }
