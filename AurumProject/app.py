2e0e102b0a7fd4203121c2563570d72ccbf9978b
gabi-tarifa <gabriel.tarifa.de.oliveira@gmail.com>
2025-08-08 00:53:51 -0300

    Correcao Cagada Silva 2.6.3

--- AurumProject/app.py
+++ AurumProject/app.py
@@ -353,6 +353,6 @@ def allowed_file(filename):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    #app.run(debug=True)
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
